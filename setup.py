056d62b1fe0fdb3b0925061ec307c8b59a0aaf7a
captain13128 <capitan.develop@gmail.com>
2020-05-06 18:44:59 +0300

    add setup.py and add MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='ethgasstation-client',
+    version='1.0.0',
+    packages=[
+        'ethgasstation_client',
+    ],
+    url='https://github.com/captain13128/ethgasstation-client.git',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    install_requires=[
+        "requests==2.22.0"
+    ]
+)
